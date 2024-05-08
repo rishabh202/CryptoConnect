26e4d0c86bbaa554e11995d59e6dc7fe3864008d
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-05-08 10:13:02 +0530

    updated all layout

--- src/App.js
+++ src/App.js
@@ -17,8 +17,19 @@ const App = () => {
           <Route path='/crypto/:coinId'element={<CryptoDetails />} />
           <Route path='/news'element={<News />} />
           </Routes></div>
-        </Layout> </div>
-      <div className='footer'> </div>
+        </Layout> 
+      <div className='footer'>
+        <Typography.Title level={5} style={{color: 'white', textAlign: "center"}}>
+          CryptoConnect <br />
+          All rights reserved</Typography.Title>
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+              </Space>            
+           </div>
+        </div>
+
     </div>
   )
 }
