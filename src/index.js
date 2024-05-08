989727bdb60be92a6c0f703c33474b274e77d2c0
Rishabh Shukla <shuklarishabh618@gmail.com>
2024-05-08 17:51:26 +0530

    updated the homepage and done basic work with redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import store from "./app/store";
+import { Provider } from "react-redux";
 import 'antd/dist/reset.css';
 
 
@@ -10,10 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 
     <Router>
-
-    <React.StrictMode>
+      <Provider store={store}>
+    {/* <React.StrictMode> */}
         <App/>
-    </React.StrictMode>
+        </Provider>
+    {/* </React.StrictMode> */}
     </Router>
 );
 
